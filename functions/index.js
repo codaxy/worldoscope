336c8ee65a6952d18a113991d42d28c13180cb32
Marko Stijak <mstijak@gmail.com>
2017-03-14 13:42:14 +0100

    ...

--- functions/index.js
+++ functions/index.js
@@ -7,10 +7,41 @@ admin.initializeApp(functions.config().firebase);
 
 exports.starAdded = functions.database.ref("/stars/{reportId}/{userId}").onWrite((event) => {
     var collectionRef = event.data.ref.parent;
-    var countRef = collectionRef.parent.child('/reports/{reportId}/starCount');
+    var countRef = functions.database.ref("/gallery/{reportId}/starCount");
 
-    return collectionRef.once('value', function (reportStars) {
-        return countRef.set(reportStars.numChildren());
+    if (!countRef.exists())
+        return null;
+
+    return collectionRef.once('value', function (stars) {
+        return countRef.set(stars ? stars.numChildren() : 0);
     });
 });
 
+exports.addToGallery = functions.database.ref("/report/{reportId}").onWrite((event) => {
+    var reportRef = event.data.ref;
+    var galleryRef = functions.database.ref("/gallery/{reportId}");
+
+    return reportRef.once('value', function (report) {
+        if (report.public) {
+            if (!galleryRef.exists()) {
+                return functions.database.ref("/stars/{reportId}")
+                    .once("value", function (stars) {
+                        return galleryRef.set({
+                            id: report.id,
+                            title: report.title,
+                            description: report.description,
+                            starCount: stars ? stars.numChildren() : 0
+                        });
+                    })
+            }
+            else {
+                return galleryRef.update({
+                    "title": report.title,
+                    "description": report.description
+                });
+            }
+        }
+        else if (galleryRef.exists())
+            return galleryRef.remove();
+    });
+});
