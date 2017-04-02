367295cf4fd89aa0660cb66227db3d7487737b74
Marko Stijak <mstijak@gmail.com>
2017-04-02 22:48:11 +0200

    ...

--- app/routes/report/Controller.js
+++ app/routes/report/Controller.js
@@ -80,8 +80,9 @@ export default class extends Controller {
     }
 
     setupAutoSave() {
-        this.addTrigger('autoSave', ['$page.report'], () => {
-            this.saveReport();
+        this.addTrigger('autoSave', ['$page.report'], (report) => {
+            if (currentUserId() == report.userId)
+                this.saveReport();
         });
     }
 
