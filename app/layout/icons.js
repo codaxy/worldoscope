03aa1247ac8cf8a46052f0d758452c54bd900681
Marko Stijak <mstijak@gmail.com>
2017-04-03 12:16:33 +0200

    ...

--- app/layout/icons.js
+++ app/layout/icons.js
@@ -1,9 +1,4 @@
 import { Icon, TreeNode } from 'cx/widgets';
 import { VDOM } from 'cx/ui';
 
-//TreeNode is loaded to register the loading icon. Figure out a better way to do that when is set to useSrc: true.
-
-Icon.registerFactory((name, props) => {
-    props.className = 'material-icons  ' + (props.className || '');
-    return <i {...props}>{name}</i>;
-});
+//TreeNode is loaded to register the loading icon. Figure out a better way to do that when is set to useSrc: true
