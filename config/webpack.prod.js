12ebc082ef7640a550de1f169b487e2a005eb060
Marko Stijak <mstijak@gmail.com>
2017-04-10 08:06:28 +0200

    ...

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -23,7 +23,7 @@ var specific = {
     },
 
     plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
+        //new webpack.optimize.UglifyJsPlugin(),
         //new BundleAnalyzerPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
