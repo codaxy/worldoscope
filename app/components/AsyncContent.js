679f41979cff9c4747eb0d3c3eb03e5c17e7414d
Marko Stijak <mstijak@gmail.com>
2017-04-10 10:06:29 +0200

    Duplicate rendering fix

--- app/components/AsyncContent.js
+++ app/components/AsyncContent.js
@@ -1,13 +1,16 @@
 import {Widget, PureContainer} from 'cx/ui';
 
 export class AsyncContent extends PureContainer {
-  initInstance(context, instance) {
-    super.initInstance(context, instance);
-    if (this.onLoadContent) {
-      this.onLoadContent().then(content => {
-        this.items = [Widget.create(content)];
-        instance.store.notify();
-      });
-    }
-  }
+	initInstance(context, instance) {
+		super.initInstance(context, instance);
+		if (!this.loaded && this.onLoadContent) {
+			this.onLoadContent().then(content => {
+				this.loaded = true;
+				this.items = [Widget.create(content)];
+				instance.setState({
+					loaded: true
+				})
+			});
+		}
+	}
 }
