e756100a34f5db01ec6b220be2617004ff094796
Marko Stijak <mstijak@gmail.com>
2017-04-27 11:18:42 +0200

    ...

--- app/routes/report/Section.js
+++ app/routes/report/Section.js
@@ -11,10 +11,11 @@ import {
 	DragSource,
 	DragHandle,
 	Menu,
-	Submenu
+	Submenu,
+	IsolatedScope
 } from "cx/widgets";
 
-import { AnimatedHeight } from "app/components";
+import {AnimatedHeight} from "app/components";
 import sectionTypes from "../../sections";
 
 export default (
@@ -28,125 +29,126 @@ export default (
 				id:bind="$section.id"
 				class={{
 					section: true,
-					edit: { bind: "$sectionData.form" }
+					edit: {bind: "$sectionData.form"}
 				}}
 				data={{
-					index: { bind: "$index" },
+					index: {bind: "$index"},
 					type: "section"
 				}}
 				hideOnDrag
 				handled
 			>
 				<AnimatedHeight loaded:expr="!{$sectionData.loading}">
-					<div visible:expr="!{$sectionData.form}">
-						<FlexRow align="center" style="margin-bottom: 10px">
-							<Heading level={3}>
-								<a
-									href:tpl="#{$section.id}"
-									text:expr="{$sectionData.title} || {$section.title} || ''"
-								/>
-							</Heading>
-							<DragHandle
-								style="cursor:move;"
-								visible:bind="editable"
-							>
-								<div tooltip="Drag &amp; drop to reorder sections">
-									<Icon name="more_horiz" />
-								</div>
-							</DragHandle>
+					<IsolatedScope bind={["$section", "$sectionData"]}>
+						<div visible:expr="!{$sectionData.form}">
+							<FlexRow align="center" style="margin-bottom: 10px">
+								<Heading level={3}>
+									<a
+										href:tpl="#{$section.id}"
+										text:expr="{$sectionData.title} || {$section.title} || ''"
+									/>
+								</Heading>
+								<DragHandle
+									style="cursor:move;"
+									visible:bind="editable"
+								>
+									<div tooltip="Drag &amp; drop to reorder sections">
+										<Icon name="more_horiz"/>
+									</div>
+								</DragHandle>
 
-							<Menu
-								visible:bind="editable"
-								horizontal
-								class="ambient-color"
-								style="margin-left: auto;display:flex;align-items:center"
-							>
+								<Menu
+									visible:bind="editable"
+									horizontal
+									class="ambient-color"
+									style="margin-left: auto;display:flex;align-items:center"
+								>
+									<Button
+										mod="hollow"
+										class="ambient-color"
+										icon="mode_edit"
+										onClick={(e, {store}) => {
+											let config = store.get("$section");
+											store.set("$sectionData.form", config);
+										}}
+									/>
+									<Submenu placement="down-left">
+										<a><Icon name="more_vert"/></a>
+										<Menu putInto="dropdown">
+											<a href="#" onClick="duplicateSection">Duplicate</a>
+											<a href="#" onClick="deleteSection">Delete</a>
+										</Menu>
+									</Submenu>
+								</Menu>
+							</FlexRow>
+							<ContentResolver
+								params:bind="$section"
+								onResolve={section => {
+									let sectionType = sectionTypes[section.type];
+									return sectionType && sectionType.view(section);
+								}}
+							/>
+						</div>
+
+						<div visible:bind="$sectionData.form">
+							<ValidationGroup valid:bind="$sectionData.formValid">
+								<Rescope bind="$sectionData.form">
+									<ContentResolver
+										params:bind="$root.$section"
+										onResolve={section => {
+											let sectionType = sectionTypes[section.type];
+											return sectionType && sectionType.config;
+										}}
+									/>
+								</Rescope>
+							</ValidationGroup>
+							<br />
+							<FlexRow hspacing>
+								<Button
+									mod="primary"
+									icon="done"
+									disabled:expr="!{$sectionData.formValid}"
+									onClick={(e, {store}) => {
+										let form = store.get("$sectionData.form");
+										store.delete("$sectionData.form");
+										store.delete("$sectionData.isNew");
+										store.set("$section", form);
+									}}
+								>
+									Save
+								</Button>
 								<Button
 									mod="hollow"
-									class="ambient-color"
-									icon="mode_edit"
-									onClick={(e, { store }) => {
-										let config = store.get("$section");
-										store.set("$sectionData.form", config);
+									onClick={(e, {store}) => {
+										store.delete("$sectionData.form");
+										let isNew = store.get("$sectionData.isNew");
+										store.delete("$sectionData.isNew");
+										if (isNew) {
+											let s = store.get("$section");
+											store.update("report.sections", sections =>
+												sections.filter(x => x != s)
+											);
+										}
 									}}
-								/>
-								<Submenu placement="down-left">
-									<a><Icon name="more_vert" /></a>
-									<Menu putInto="dropdown">
-										<a href="#" onClick="duplicateSection">Duplicate</a>
-										<a href="#" onClick="deleteSection">Delete</a>
-									</Menu>
-								</Submenu>
-							</Menu>
-						</FlexRow>
-						<ContentResolver
-							params:bind="$section"
-							onResolve={section => {
-								let sectionType = sectionTypes[section.type];
-								return sectionType && sectionType.view(section);
-							}}
-						/>
-					</div>
+								>
+									Cancel
+								</Button>
 
-					<div visible:bind="$sectionData.form">
-						<ValidationGroup valid:bind="$sectionData.formValid">
-							<Rescope bind="$sectionData.form">
-								<ContentResolver
-									params:bind="$root.$section"
-									onResolve={section => {
-										let sectionType = sectionTypes[section.type];
-										return sectionType && sectionType.config;
-									}}
-								/>
-							</Rescope>
-						</ValidationGroup>
-						<br />
-						<FlexRow hspacing>
-							<Button
-								mod="primary"
-								icon="done"
-								disabled:expr="!{$sectionData.formValid}"
-								onClick={(e, { store }) => {
-									let form = store.get("$sectionData.form");
-									store.delete("$sectionData.form");
-									store.delete("$sectionData.isNew");
-									store.set("$section", form);
-								}}
-							>
-								Save
-							</Button>
-							<Button
-								mod="hollow"
-								onClick={(e, { store }) => {
-									store.delete("$sectionData.form");
-									let isNew = store.get("$sectionData.isNew");
-									store.delete("$sectionData.isNew");
-									if (isNew) {
+								<Button
+									mod="hollow"
+									icon="delete"
+									style="margin-left: auto"
+									confirm="Are you sure that you want to delete this section?"
+									onClick={(e, {store}) => {
 										let s = store.get("$section");
 										store.update("report.sections", sections =>
 											sections.filter(x => x != s)
 										);
-									}
-								}}
-							>
-								Cancel
-							</Button>
-
-							<Button
-								mod="hollow"
-								icon="delete"
-								style="margin-left: auto"
-								confirm="Are you sure that you want to delete this section?"
-								onClick={(e, { store }) => {
-									let s = store.get("$section");
-									store.update("report.sections", sections =>
-										sections.filter(x => x != s)
-									);
-								}}
-							/>
-						</FlexRow>
-					</div>
-
+									}}
+								/>
+							</FlexRow>
+						</div>
+					</IsolatedScope>
 				</AnimatedHeight>
 			</DragSource>
 		</Sandbox>
